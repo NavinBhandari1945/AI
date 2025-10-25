780c58e06bacbab2b6d97ba2b2a1dccabe7190da
NavinBhandari1945 <navinbhandari2002@gmail.com>
2025-10-25 20:08:47 +0545

    fix overflow error

--- 03_document_scanner_text_recognize/app/lib/views/home/home_screen.dart
+++ 03_document_scanner_text_recognize/app/lib/views/home/home_screen.dart
@@ -10,11 +10,45 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+
+    var widthval=MediaQuery.of(context).size.width;
+    var heightval=MediaQuery.of(context).size.height;
+    var shortestval=MediaQuery.of(context).size.shortestSide;
     return Scaffold(
       appBar: AppBar(
         title: Text("Home Screen"),
       ),
       body: Container(
+        child: Column(
+          children:
+          [
+
+            Card(
+              child: Container(
+                height: heightval*0.10,
+                color:Colors.green,
+                child: Row(
+                  children:
+                  [
+
+                    InkWell(
+                      child: Icon(
+                          Icons.camera,
+                        color: Colors.red,
+                        size: shortestval*0.15,
+                      ),
+                      onTap: (){
+
+                      },
+                    ),
+
+                  ],
+                ),
+              ),
+            )
+
+          ],
+        ),
 
       ),
     );
